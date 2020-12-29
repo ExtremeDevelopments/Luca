44a25ccf99d1b836743f373c3bb0867338d936d8
VoltrexMaster <62040526+VoltrexMaster@users.noreply.github.com>
2020-12-29 09:23:16 +0330

    Cleanups, improvements and better implementations
    Cleaned up redundant code, improved readability and implemented better info and values.

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -1,69 +1,66 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const pkg = require(__basedir + '/package.json');
 const { mem, cpu, os } = require('node-os-utils');
-const moment = require('moment');
-const { oneLine, stripIndent } = require('common-tags');
-const { Shard } = require('discord.js');
+const { stripIndent } = require('common-tags');
+const parseMs = require('parse-ms');
 
 module.exports = {
-    name: "botinfo",
-    aliases: ["bi"],
-    run: async(client, message) => {
-
-    const embed = new MessageEmbed()
+    name: 'botinfo',
+    aliases: ['bi'],
+    run: async (client, message) => {
+      const embed = new MessageEmbed()
       .setTitle('Fetching Information (Please Wait)')
       .setColor('#a8e3e7');
 
-    const d = moment.duration(message.client.uptime);
-    const days = (d.days() == 1) ? `${d.days()} day` : `${d.days()} days`;
-    const hours = (d.hours() == 1) ? `${d.hours()} hour` : `${d.hours()} hours`;
+    const uptime = Object.entries(parseMs(client.uptime)).filter(([key, value]) => key !== "milliseconds" && value > 0).map(([key, value]) => `${value} ${key[0].toUpperCase()}${value === 1 ? key.slice(1, -1) : key.slice(1)}`).join(', ');
     const { totalMemMb, usedMemMb } = await mem.info();
     const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
     const msg = await message.channel.send(embed);
-    const timestamp = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp;   
+    const timestamp = message.editedTimestamp || message.createdTimestamp;   
 
-    const details = stripIndent`
+    const team = stripIndent`
   Main Developers
   • Extreme#1000
-  • DevNoah#0001`;
+  • DevNoah#0001
+  • Voltrex Master#0001`;
   
-    const info2 = stripIndent`
-    WS Ping ----- ${Math.round(message.client.ws.ping)}ms
-    Uptime ------ ${days} and ${hours}
+    const mainInfo = stripIndent`
+    WS Ping ----- ${Math.round(client.ws.ping)}ms
+    Uptime ------ ${uptime}
     Version ----- ${pkg.version}
-    Library ----- Discord.js v12.4.1
-    Enviorment -- Node.js v14.4.0
+    Library ----- Discord.js v${Discord.version}
+    Enviorment -- Node.js ${process.version}
     -
-    Servers ----- ${message.client.guilds.cache.size}
-    Users ------- ${message.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
-    Shards ------ ${message.client.shard.count}
-    Channels ---- ${message.client.channels.cache.size}
-    Commands ---- ${message.client.commands.size}
-    Aliases ----- ${message.client.aliases.size}
+    Servers ----- ${client.guilds.cache.size.toLocaleString()}
+    Users ------- ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
+    Shards ------ ${client.shard.count.toLocaleString()}
+    Channels ---- ${client.channels.cache.size.toLocaleString()}
+    Commands ---- ${client.commands.size.toLocaleString()}
+    Aliases ----- ${client.aliases.size.toLocaleString()}
   `;
 
-    const stats1 = stripIndent`
-    Platform -- ${await os.oos()}
+    const additionalInfo = stripIndent`
+    Platform -- ${(await os.oos())}
     CPU ------- ${cpu.model()}
     `;
 
-  const stats2 = stripIndent`
-Rss --------------- ${(process.memoryUsage().rss / (1024 * 1024)).toFixed()}MB
-HeapTotal --------- ${(process.memoryUsage().heapTotal / (1024 * 1024)).toFixed()}MB
-HeapUsed ---------- ${(process.memoryUsage().heapUsed / (1024 * 1024)).toFixed()}MB
-External ---------- ${(process.memoryUsage().external / (1024 * 1024)).toFixed()}MB
-  `;
+  const memoryUsage = Object.entries(process.memoryUsage()).map(([key, value]) => `${`${key[0].toUpperCase()}${key.slice(1)}`.padEnd(19, '-')} ${value / 1024 / 1024}MB`).join('\n');
+
     embed
-    .setAuthor(`${message.member.user.tag}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
-    .setTitle(`Sailboat's Information `)
-        .addField('Bot Information', `\`\`\`css\n${info2}\`\`\``, true)
-        .addField('Sailboat\'s Team', `\`\`\`css\n${details}\`\`\``, true)
-        .addField('System Statistics', `\`\`\`css\n${stats1}\`\`\``, false)
-        .addField('Memory Usage', `\`\`\`css\n${stats2}\`\`\``, false)
-        .setFooter(`Sailboat - ${pkg.version}`)
-      .setTimestamp()
-      .setColor('#a8e3e7')
-      msg.edit(embed);
-  }
-}
+    .setAuthor(message.author.tag, message.author.displayAvatarURL({
+       dynamic: true,
+       size: 1024,
+       format: 'png'
+    }))
+    .setTitle(`${client.user.username}'s information`)
+    .addField('Bot information', `\`\`\`css\n${mainInfo}\n\`\`\``, false)
+    .addField(`${client.user.username}'s team`, `\`\`\`css\n${team}\n\`\`\``, false)
+    .addField('System statistics', `\`\`\`css\n${additionalInfo}\n\`\`\``, false)
+    .addField('Memory usage', `\`\`\`css\n${memoryUsage}\n\`\`\``, false)
+    .setFooter(`${client.user.username} - ${pkg.version}`)
+    .setTimestamp()
+    .setColor('#a8e3e7');
     
+    return msg.edit(embed);
+  }
+};
